e6a8d8efb795aadbb8c91373c5ef3750d46193d7
marbiru <code@uribram.com>
2015-05-12 13:18:48 +0100

    Added functioning points-counter
    increments with time; later this will decrement from some fixed number
    for each level

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
-$(function(){
-	$("#points_counter").text("hello");
-  });
-
-function run_counter() {
-		var counter = 0;
-		setInterval(function () {
-			++counter;
-		}, 1000);
-		points_counter.innerHTML = counter;
-    	};
-
+$(function() {
+    var cnt = 0;
+		var counter = setInterval(function() {
+                    $( "#points_counter").html( cnt );
+                    cnt++;
+                	},
+ 					10);
 });
