12e6212d27881a3b865103d907efeb5a210bf530
marbiru <uribram@gmail.com>
2015-05-12 13:01:49 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-var counter = 0;
-
-setInterval(function () {
-  ++counter;
-}, 1000);
-
 $(function(){
-	$("#points_counter").text(counter);
-});
+	$("#points_counter").text("hello");
+  });
 
+function run_counter() {
+		var counter = 0;
+		setInterval(function () {
+			++counter;
+		}, 1000);
+		points_counter.innerHTML = counter;
+    	};
+
+});
