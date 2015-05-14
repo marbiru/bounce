fa9d6805b97c5fb510296a4127c7eff0dcefbece
marbiru <code@uribram.com>
2015-05-14 12:11:12 +0100

    Revert e428598..bb7fb1a
    This rolls back to commit e428598277108fc3bc81d88656ee265b7aa5001a.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# bounce-escape
-A little game where you need to get a ball to escape the canvas by bouncing it from the ground up
+# bounce
+A little game where you need to slow a bouncing ball by just the right amount
 
 + a ball is bouncing under force of gravity
-+ you press "up" to increase gravity -- only do this when the ball is heading downwards
-+ try to get the ball to bounce above the canvas
++ player presses "up" button repeatedly to slow down the ball (using a little "blaster" from underneath the ball)
++ the trick is to get the ball to stop as quickly as possible... but no quicker. If the ball ever starts going upwards when it shouldn't then the ball lifts off (what happens when the ball is bouncing upwards? Maybe then the player has to press down instead. Or maybe the ball never lifts off, the trick is just to press up/down at all the right times)
++ in future levels, gravity is higher (or similar tricks)
--- ball.js
+++ ball.js
@@ -1,27 +1,22 @@
 // from http://cssdeck.com/labs/lets-make-a-bouncing-ball-in-html5-canvas
 
 // Now some basic canvas stuff. Here we'll make a variable for the canvas and then initialize its 2d context for drawing
-var canvas = document.getElementById("canvas");
-
-var canvas_context = canvas.getContext("2d");
+var canvas = document.getElementById("canvas"),
+		ctx = canvas.getContext("2d");
 
 // Now setting the width and height of the canvas
-var canvas_width = 350;
-
-var canvas_height = 450;
+var W = 350,
+		H = 450;
 
 // Applying these to the canvas element
-canvas.height = canvas_height; 
-canvas.width = canvas_width;
+canvas.height = H; canvas.width = W;
 
 // First of all we'll create a ball object which will contain all the methods and variables specific to the ball.
 // Lets define some variables first
 
-var ball = {};
-
-var gravity = 0.2;
-
-var bounce_factor = 0.7;
+var ball = {},
+		gravity = 0.2,
+		bounceFactor = 0.7;
 
 // The ball object
 // It will contain the following details
@@ -31,30 +26,30 @@ var bounce_factor = 0.7;
 // 4) the method to draw or paint it on the canvas
 
 ball = {
-	x: canvas_width/2,
-	y: 300,
-
+	x: W/2,
+	y: 50,
+	
 	radius: 15,
 	color: "red",
 	
 	// Velocity components
-	vx: 0.2,
+	vx: 0,
 	vy: 1,
 	
 	draw: function() {
 		// Here, we'll first begin drawing the path and then use the arc() function to draw the circle. The arc function accepts 6 parameters, x position, y position, radius, start angle, end angle and a boolean for anti-clockwise direction.
-		canvas_context.beginPath();
-		canvas_context.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
-		canvas_context.fillStyle = this.color;
-		canvas_context.fill();
-		canvas_context.closePath();
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+		ctx.fillStyle = this.color;
+		ctx.fill();
+		ctx.closePath();
 	}
 };
 
 // When we do animations in canvas, we have to repaint the whole canvas in each frame. Either clear the whole area or paint it with some color. This helps in keeping the area clean without any repetition mess.
 // So, lets create a function that will do it for us.
 function clearCanvas() {
-	canvas_context.clearRect(0, 0, canvas_width, canvas_height);
+	ctx.clearRect(0, 0, W, H);
 }
 
 // A function that will update the position of the ball is also needed. Lets create one
@@ -64,41 +59,42 @@ function update() {
 	
 	// Now, lets make the ball move by adding the velocity vectors to its position
 	ball.y += ball.vy;
-	ball.x += ball.vx;
 	// Ohh! The ball is moving!
 	// Lets add some acceleration
 	ball.vy += gravity;
 	$(document).keydown(function(e) {
     	switch(e.which) {
-        	case 40: // down
+    		case 37: // left
+        	break;
+        	
+        	case 38: // up
         	ball.vy = ball.vy + 0.01;
+        	$( "#dummy_text" ).html( "dummy up" );
+        	//$( "#dummy_text" ).html( "baaaaa" );
         	break;
-        	
-        	case 37: // left
-        	ball.vx = ball.vx - 0.01;
-        	break;
-        	
+
         	case 39: // right
-        	ball.vx = ball.vx + 0.01;
         	break;
-        	
+
+        	case 40: // down
+        	ball.vy = ball.vy - 0.01;
+        	$( "#dummy_text" ).html( "dummy down" );
+        	break;
+
         	default: return; // exit this handler for other keys
     	}	
     	e.preventDefault(); // prevent the default action (scroll / move caret)
 	});
 	//Perfect! Now, lets make it rebound when it touches the floor
-	if(ball.y + ball.radius > canvas_height) {
+	if(ball.y + ball.radius > H) {
 		// First, reposition the ball on top of the floor and then bounce it!
-		ball.y = canvas_height - ball.radius;
-		ball.vy *= -bounce_factor;
+		ball.y = H - ball.radius;
+		ball.vy *= -bounceFactor;
 		// The bounceFactor variable that we created decides the elasticity or how elastic the collision will be. If it's 1, then the collision will be perfectly elastic. If 0, then it will be inelastic.
-	if(ball.x + ball.radius > canvas_width) {
-		// First, reposition the ball on top of the floor and then bounce it!
-		ball.x = canvas_width - ball.radius;
-		ball.vx *= -bounce_factor;
 	}
 }
 
 // Now, the animation time!
 // in setInterval, 1000/x depicts x fps! So, in this casse, we are aiming for 60fps for smoother animations.
 setInterval(update, 1000/60);
+
