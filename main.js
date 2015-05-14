09aaece3fba6f3ea4a18865f2a769b31f88b6d03
marbiru <code@uribram.com>
2015-05-14 12:57:22 +0100

    Points counter stops at  1
    doesn't go negative

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 // the timer/points-counter: starts at some number of points and counts down to zero. The faster you complete the level, the more points you'll have
 
 $(function() {
-	var count = 5000
+	var start_points = 5000;
     var time_elapsed = 0;
-		var run_counter = setInterval(function() {
-                    $( "#points_counter" ).html( count - time_elapsed );
-                    time_elapsed++;
-                	}, 10);
+		setInterval(function() {
+            $( "#points_counter" ).html( start_points - time_elapsed );
+                if (time_elapsed + 1 < start_points) {
+                        time_elapsed += 1;
+            }
+        }, 10);
 });
-
 
 // when you press the up arrow the ball slows down
 
